e718c7a94d8f798aa9b37e80e1d905a94b397b6a
Edward Whelan <vroxing@gmail.com>
2018-10-18 15:02:48 -0400

    add POST

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,46 @@
-console.log('is this thing on');
+//=======================================
+//DOM selections
+const orderForm = document.querySelector('[data-form]');
+
+
+//=======================================
+// Helper functions
+//=======================================
+
+function handleSubmit(event){
+    event.preventDefault();
+    console.log('coffee get');
+    console.log(event.target);
+    // AJAX that thing
+    //call fetch
+    // pass it the url
+    // and an object with a method and a body
+    const url = event.target.action;
+    const method = event.target.method;
+    const elements = event.target.elements;
+    const data = {
+        strength: elements.strength.value,
+        flavor: elements.flavor.value,
+        size: elements.size.value,
+        coffee: elements.coffee.value,
+        emailAddress: elements.emailAddress.value
+    };
+
+    fetch(url, {
+        method: method,
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(data)
+
+    })
+
+
+}
+
+
+//=======================================
+// Main Event Listeners
+
+orderForm.addEventListener('submit', handleSubmit)
